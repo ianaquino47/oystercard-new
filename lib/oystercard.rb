3da4907632ba546d97f828deee803ab161f847bf
iAquino47 <ianaquino47@gmail.com>
2019-07-02 14:40:03 +0100

    stage 11 completed

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -4,29 +4,33 @@ class Oystercard
   def initialize(balance = 0)
     @balance = balance
     @in_journey = false
+    @entry_station = nil
   end
 
-  attr_reader :balance
-
+  attr_reader :balance, :entry_station
   def top_up(amount)
     raise "Error: Maximum balance is £#{MAXIMUM_BALANCE}" if @balance + amount > MAXIMUM_BALANCE
     @balance += amount
   end
 
-  def touch_in
+  def touch_in(station = '')
     raise "Error: insufficient funds. Balance is #{@balance}." if @balance < MINIMUM_FARE
+    @entry_station = station
     @in_journey = true
   end
 
   def touch_out
     @in_journey = false
     deduct(MINIMUM_FARE)
+    @entry_station = nil
   end
 
   def in_journey?
-    @in_journey
+    @entry_station != nil
   end
+
   private
+
   def deduct(amount)
     @balance -= amount
   end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,48 +1,58 @@
+# frozen_string_literal: true
+
 require 'oystercard'
 describe Oystercard do
-  it "initialises with a default balance of 0" do
+  it 'initialises with a default balance of 0' do
     expect(subject.balance).to eq 0
   end
 
-  it "the balance can be topped up with top_up method" do
+  it 'the balance can be topped up with top_up method' do
     subject.top_up(5)
     expect(subject.balance).to eq 5
   end
 
-  it "the maximum balance cannot exceed 90" do
-    expect {subject.top_up(91)}.to raise_error("Error: Maximum balance is £#{Oystercard::MAXIMUM_BALANCE}")
+  it 'the maximum balance cannot exceed 90' do
+    expect { subject.top_up(91) }.to raise_error("Error: Maximum balance is £#{Oystercard::MAXIMUM_BALANCE}")
   end
 
-#  it "the fare amount gets deducted from the total balance" do
-#    subject.top_up(50)
-#    expect(subject.deduct(3)).to eq 47
-#  end
+  #  it "the fare amount gets deducted from the total balance" do
+  #    subject.top_up(50)
+  #    expect(subject.deduct(3)).to eq 47
+  #  end
 
-  it { should_not be_in_journey } #used predicate method matcher
+  it { should_not be_in_journey } # used predicate method matcher
 
-  it "touching in changes in_journey to true" do
+  it 'touching in changes in_journey to true' do
     subject.top_up(2)
     subject.touch_in
     expect(subject.in_journey?).to eq true
   end
-  
 
-  it "touching out changes in_journey to false" do
+  it 'touching out changes in_journey to false' do
     subject.top_up(2)
     subject.touch_in
     subject.touch_out
     expect(subject.in_journey?).to eq false
   end
   it "checks for minimum balance on touch in and raises error if balance is < minimum fare: #{Oystercard::MINIMUM_FARE}" do
-    expect { subject.touch_in}.to raise_error("Error: insufficient funds. Balance is #{subject.balance}.")
+    expect { subject.touch_in }.to raise_error("Error: insufficient funds. Balance is #{subject.balance}.")
   end
   it "checks for minimum balance on touch in and does not raise error if balance is >= minimum fare: #{Oystercard::MINIMUM_FARE}" do
     subject.top_up(Oystercard::MINIMUM_FARE)
     expect(subject.touch_in).to eq true
   end
-  it "deducts minimumfare from balance upon touch-out" do
+  it 'deducts minimumfare from balance upon touch-out' do
     subject.top_up(2)
     subject.touch_in
-    expect {subject.touch_out}.to change{subject.balance}.by(-1)
+    expect { subject.touch_out }.to change { subject.balance }.by(-1)
   end
+
+  it 'card remembers entry station after touch in' do
+    subject.top_up(2)
+    station = double(:fakestation)
+    allow(station).to receive(:name).and_return("Aldgate East")
+    subject.touch_in(station.name)
+    expect(subject.entry_station).to eq "Aldgate East"
+  end
+
 end
